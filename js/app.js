3dbfc38a3a47c1b7a0792c38b5cf55274d345f7a
Daniel Joseph Merton Egan <CATHOLICTIMELINE@gmail.com>
2025-01-19 20:45:04 -0500

    I figured out the problem with the else / if. I had = instead of == to compare the numbers. Secondly, By God's grace I got the array to display my guesses.

--- js/app.js
+++ js/app.js
@@ -1,15 +1,16 @@
 "use strict";
 const guessInputEl = document.getElementById("guess-input")
-
 const subMitBtn = document.getElementById("submit-btn");
+
 
 const guess = document.getElementById("guess-input");
 const randomNumber = Math.floor(Math.random() * 10) + 1;
+let array = [];
 
 function numberGuessed(){
   let guess = document.getElementById("guess-input").value;
 
-  if (guess = randomNumber){
+  if (guess == randomNumber){
     document.getElementById("guess-message").innerHTML = `${"You Won!"}`;
   }
   else if (guess > randomNumber){
@@ -30,24 +31,15 @@ function computerGuess(){
   document.getElementById("computer-guess").innerText = randomNumber;
 }
 
+function guessHistory(){
+array.push(guess.value);
+document.getElementById("guess-history").innerHTML = array;
 
-// <h1>Enter a number</h1>
-// <input type="number" id="inputNumber" placeholder="Enter a number">
-// <button onclick="displayNumber()">Display Number</button>
-
-// <p id="output"></p>
-
-// <script>
-//     function displayNumber() {
-//         const number = document.getElementById("inputNumber").value;
-//         document.getElementById("output").innerText = "You entered: " + number;
-//     }
+}
 
 
-
-// - [ ] The span with the id of `current-guess` should output the guess that was just submitted.
-// - [ ] The span with the id of `computer-guess` should output the guess that was randomly generated.
 // - [ ] The span with the id of `guess-history` should output your guess history.
+
 // - [ ] The "Submit Guess" button should be enabled at the beginning of the game, but when the game is over it should be disabled.
 // - [ ] The "Restart" button should be disabled at the beginning of the game, but when the game is over it should be enabled.
 // - [ ] When the "Restart" button is clicked, all values and text output should reset.
@@ -57,6 +49,7 @@ function render(){
   numberGuessed();
   displayGuess();
   computerGuess();
+  guessHistory();
 }
 
 
