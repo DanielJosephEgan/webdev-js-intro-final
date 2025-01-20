797290369ef3e6801fbbb1cff7d3c1491d4d55ae
Daniel Joseph Merton Egan <CATHOLICTIMELINE@gmail.com>
2025-01-19 23:58:56 -0500

    I mostly have it finished with a flew glitches

--- js/app.js
+++ js/app.js
@@ -1,17 +1,25 @@
 "use strict";
 const guessInputEl = document.getElementById("guess-input")
 const subMitBtn = document.getElementById("submit-btn");
+const reStartBtn = document.getElementById("restart-btn");
 
 
 const guess = document.getElementById("guess-input");
 const randomNumber = Math.floor(Math.random() * 10) + 1;
 let array = [];
+const restartBtn = document.getElementById("restart-btn");
+
+
 
 function numberGuessed(){
   let guess = document.getElementById("guess-input").value;
 
   if (guess == randomNumber){
     document.getElementById("guess-message").innerHTML = `${"You Won!"}`;
+    document.getElementById("computer-guess").innerText = randomNumber;
+    document.getElementById("restart-btn").disabled = false;
+    document.getElementById("computer-guess").innerText = randomNumber;
+
   }
   else if (guess > randomNumber){
     document.getElementById("guess-message").innerHTML = `${"Opps! Too High!"}`;
@@ -20,40 +28,51 @@ function numberGuessed(){
   else if (guess < randomNumber){
     document.getElementById("guess-message").innerHTML = `${"Uh Oh! Too Low!"}`;
   }
+
+
 }
+
 
 function displayGuess(){
   let guess = document.getElementById("guess-input").value;
   document.getElementById("current-guess").innerText = guess;
 }
 
-function computerGuess(){
-  document.getElementById("computer-guess").innerText = randomNumber;
-}
+
+
 
 function guessHistory(){
+  if (array.length < 3){
 array.push(guess.value);
 document.getElementById("guess-history").innerHTML = array;
 
+
+if (array.length == 3){
+  document.getElementById("guess-message").innerHTML = `${"You lose!"}`;
+document.getElementById("restart-btn").disabled = false;
+  
+}
+
+}
 }
 
 
-// - [ ] The span with the id of `guess-history` should output your guess history.
+restartBtn.addEventListener("click", function() {
+  location.reload();
+});
 
-// - [ ] The "Submit Guess" button should be enabled at the beginning of the game, but when the game is over it should be disabled.
-// - [ ] The "Restart" button should be disabled at the beginning of the game, but when the game is over it should be enabled.
-// - [ ] When the "Restart" button is clicked, all values and text output should reset.
 
 
 function render(){
   numberGuessed();
   displayGuess();
-  computerGuess();
   guessHistory();
 }
-
-
 
 subMitBtn.addEventListener("click", function(){
   render();
 });
+
+restartBtn.addEventListener("click", function(){
+  location.reload();
+})
